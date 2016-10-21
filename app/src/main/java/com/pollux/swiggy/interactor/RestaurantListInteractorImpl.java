3275dda78f240733ad7081321a626e0a05bfe55f
sreekumar.av <sreekumar.av@ymedialabs.com>
2016-10-21 13:28:18 +0530

    Moved to constant

--- app/src/main/java/com/pollux/swiggy/interactor/RestaurantListInteractorImpl.java
+++ app/src/main/java/com/pollux/swiggy/interactor/RestaurantListInteractorImpl.java
@@ -19,6 +19,11 @@ import rx.functions.Func1;
  * Get Restaurant list and their chain
  */
 public class RestaurantListInteractorImpl implements RestaurantListInteractor {
+
+    private static final String SEP = ",";
+    private static final String AT = " at ";
+    private static final String NEW_LINE = "\n";
+
     @Override
     public Observable<Restaurant> getRestaurantListInteractorList(int pageNo) {
         return SwiggyService.getSwiggyService().getRestaurantList(pageNo).map(new Func1<Restaurant, Restaurant>() {
@@ -32,7 +37,7 @@ public class RestaurantListInteractorImpl implements RestaurantListInteractor {
                                 rest.cuisineFormatted = "";
                                 final List<String> cuisines = rest.getCuisine();
                                 for (String cuisine : cuisines) {
-                                    rest.cuisineFormatted += cuisine + ",";
+                                    rest.cuisineFormatted += cuisine + SEP;
                                 }
                                 rest.cuisineFormatted = rest.cuisineFormatted.substring(0, rest.cuisineFormatted.length() - 1);
 
@@ -40,7 +45,7 @@ public class RestaurantListInteractorImpl implements RestaurantListInteractor {
                                 if (chain != null && chain.size() > 0) {
                                     rest.chainRestuarntFormatted = "";
                                     for (Restaurant.Restaurants restaurants1 : chain) {
-                                        rest.chainRestuarntFormatted += restaurants1.getName() + " at " +restaurants1.getCity()  + "\n";
+                                        rest.chainRestuarntFormatted += restaurants1.getName() + AT + restaurants1.getCity() + NEW_LINE;
                                     }
                                 }
                             }
